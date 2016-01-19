babfd35741a9b19a23a57a4c02b925e75db4e5b8
Aiden Montgomery <aiden@constructivecoding.com>
2016-01-19 13:46:17 +1100

    Created simple function

---
+++ SwiftLogger/SwiftLogger/Logger.swift
@@ -0,0 +1,13 @@
+//
+//  Logger.swift
+//  SwiftLogger
+//
+//  Created by Aiden Montgomery on 19/01/2016.
+//  Copyright © 2016 Constructive Coding. All rights reserved.
+//
+
+import Foundation
+
+public func Log(message: String) {
+    print(message)
+}
---
+++ SwiftLogger/SwiftLogger/SwiftLogger.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLogger.h
+//  SwiftLogger
+//
+//  Created by Aiden Montgomery on 19/01/2016.
+//  Copyright © 2016 Constructive Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLogger.
+FOUNDATION_EXPORT double SwiftLoggerVersionNumber;
+
+//! Project version string for SwiftLogger.
+FOUNDATION_EXPORT const unsigned char SwiftLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLogger/PublicHeader.h>
+
+
